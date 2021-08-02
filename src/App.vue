11d6c3acf2d15b6c254126f144e00272e6db4555
Sam McCarthy <samrmccarthy6@gmail.com>
2021-08-02 22:27:12 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="flex">
+      <div>
+      </div>
+      <div>
+        <h1 class="text-5xl font-large text-yellow">Hi, I'm Celine</h1>
+        <h2 class="text-gray-500">Experienced Product Manager and Marketer</h2>
+        <h3>Product Manager / Brand Manager / Marketer</h3>
+        <p>
+          Content marketing is an approach that helps your brand build long-term relationships with a user and gain
+          his trust. I know everything about content marketing and its techniques, and I will be happy to help you
+          promote your project.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</style>

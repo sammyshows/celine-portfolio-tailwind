61ff934d995426532ed08817e4f86ef5d0981db8
Sam McCarthy <samrmccarthy6@gmail.com>
2021-08-03 00:21:42 +1000

    Created the home page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    <div class="flex">
-      <div>
+  <div class="bg-gray-50 px-40">
+    <div class="flex h-screen">
+      <div class="flex items-center justify-end w-full pr-14">
+        <div class="relative h-auto w-auto">
+          <img src="./assets/celine-portrait.png" alt="celine-portrait" class="relative h-full w-full max-h-80 max-w-xs rounded-full border-2 border-green-600 z-10">
+          <div class="absolute top-1/2 right-1/2 z-0 bg-green-400 h-3/5 w-3/5 rounded-full bg-gradient-to-tr from-green-600 to-green-200"></div>
+          <div class="absolute bottom-1/3 left-1/3 z-0 h-4/5 w-4/5 rounded-full bg-gradient-to-tr from-green-600 to-green-200"></div>
+        </div>
       </div>
-      <div>
-        <h1 class="text-5xl font-large text-yellow">Hi, I'm Celine</h1>
-        <h2 class="text-gray-500">Experienced Product Manager and Marketer</h2>
-        <h3>Product Manager / Brand Manager / Marketer</h3>
+      <div class="flex flex-col justify-center pl-20">
+        <h1 class="text-6xl font-semibold tracking-wide text-green-600 pb-2">Hi, I'm Celine</h1>
+        <h2 class="text-4xl text-green-400 pb-2">Experienced Product Manager</h2>
+        <h3 class="text-2xl pb-2">Product Manager / Brand Manager / Marketer</h3>
         <p>
           Content marketing is an approach that helps your brand build long-term relationships with a user and gain
           his trust. I know everything about content marketing and its techniques, and I will be happy to help you
@@ -14,17 +19,21 @@
         </p>
       </div>
     </div>
+
+    <div class="h-screen">
+
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    Navbar
   }
 })
 </script>
